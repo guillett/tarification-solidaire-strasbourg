d6e207d4c2c6aa73a8817359d5a00b136586a5a0
Thomas Guillet <thomas@palya.fr>
2023-09-19 16:21:39 +0200

    Ajoute prettier

--- server/front/pages/grist.js
+++ server/front/pages/grist.js
@@ -2,11 +2,18 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function extractScenarios(columnNames) {
-  return columnNames.filter(name => {
-    return name != "id" && name != "QF" && name != "QF_bis" && !name.endsWith("_reduit")
-  }).map(name => {
-    return {name}
-  })
+  return columnNames
+    .filter((name) => {
+      return (
+        name != 'id' &&
+        name != 'QF' &&
+        name != 'QF_bis' &&
+        !name.endsWith('_reduit')
+      )
+    })
+    .map((name) => {
+      return { name }
+    })
 }
 
 export default function Home() {
@@ -25,20 +32,22 @@ export default function Home() {
   const [timer, setTimer] = useState(null)
 
   useEffect(() => {
-    window.grist.ready();
-    window.grist.onRecord(record => {
+    window.grist.ready()
+    window.grist.onRecord((record) => {
       setRecord(Object.keys(record))
       if (true || !scenarios.length) {
         setScenarios(extractScenarios(Object.keys(record)))
       }
-    });
-    window.grist.onRecords(records => {
+    })
+    window.grist.onRecords((records) => {
       setRecords(JSON.stringify(records, null, 2))
       if (true || !scenarios.length) {
         setScenarios(extractScenarios(Object.keys(records[0])))
       }
     })
-    window.grist.onOptions((options, interaction) => setOptions(JSON.stringify({options, interaction}, null, 2)))
+    window.grist.onOptions((options, interaction) =>
+      setOptions(JSON.stringify({ options, interaction }, null, 2))
+    )
   }, [scenarios.length])
 
   useEffect(() => {
@@ -49,49 +58,64 @@ export default function Home() {
     const start = new Date()
     setStartFetchAt(start)
     const interval = setInterval(() => {
-      const d = Math.round((new Date() - start)/1000) 
+      const d = Math.round((new Date() - start) / 1000)
       setTimer(`(${d}s.)`)
     }, 100)
     try {
-      const host = process.env.NODE_ENV === "production" ? "" : "http:localhost:5000"
-      const url = `${host}/fetch?scenario=${scenario}&qf=${useFiscalQF ? "fiscal": "caf"}`
+      const host =
+        process.env.NODE_ENV === 'production' ? '' : 'http:localhost:5000'
+      const url = `${host}/fetch?scenario=${scenario}&qf=${
+        useFiscalQF ? 'fiscal' : 'caf'
+      }`
       const response = await axios.get(url)
       const data = response.data
       setRecettes(data.table)
-    } catch(e) {
+    } catch (e) {
       throw e
     } finally {
       setStartFetchAt(null)
-      clearInterval(interval)      
+      clearInterval(interval)
     }
   }
 
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <div dangerouslySetInnerHTML={{__html:recettes}} />
+      <div dangerouslySetInnerHTML={{ __html: recettes }} />
       <div className="flex-col w-full max-w-5xl items-center justify-between font-mono text-sm">
         <h1 className="text-xl">Simulation</h1>
         <fieldset className="flex flex-col p-2">
           <legend className="text-lg">Scénario</legend>
-          {scenarios.map(scenario => <label key={scenario.name}><input
-            type="radio"
-            name="scenario"
-            onChange={e => setScenario(e.target.value)}
-            value={scenario.name}
-            /> {scenario.name}</label>)}
+          {scenarios.map((scenario) => (
+            <label key={scenario.name}>
+              <input
+                type="radio"
+                name="scenario"
+                onChange={(e) => setScenario(e.target.value)}
+                value={scenario.name}
+              />{' '}
+              {scenario.name}
+            </label>
+          ))}
         </fieldset>
-        <div className="p-2"><label className="text-lg">
-          <input type="checkbox"
-          value={useFiscalQF}
-          onChange={e => setUseFiscalQF(e.target.checked)}/> QF Fiscal
-        </label>
+        <div className="p-2">
+          <label className="text-lg">
+            <input
+              type="checkbox"
+              value={useFiscalQF}
+              onChange={(e) => setUseFiscalQF(e.target.checked)}
+            />{' '}
+            QF Fiscal
+          </label>
         </div>
         <button
           disabled={!canSubmit}
-          className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${canSubmit ? "hover:bg-blue-700" : "opacity-50 cursor-not-allowed"}`}
+          className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${
+            canSubmit ? 'hover:bg-blue-700' : 'opacity-50 cursor-not-allowed'
+          }`}
           onClick={fetchResults}
-          >Calculer {timer}</button>
+        >
+          Calculer {timer}
+        </button>
       </div>
     </main>
   )
--- server/front/pages/index.js
+++ server/front/pages/index.js
@@ -1,74 +1,107 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-export const getStaticProps = (async (context) => {
-  const domain = process.env.NODE_ENV == "production" ? "" : "http://127.0.0.1:8000"
+export const getStaticProps = async (context) => {
+  const domain =
+    process.env.NODE_ENV == 'production' ? '' : 'http://127.0.0.1:8000'
   return { props: { domain } }
-})
+}
 
 function isOk(email) {
   return email?.endsWith('@strasbourg.eu')
 }
 
-export default function Home({domain}) {
-  const [email, setEmail] = useState()
+export default function Home({ domain }) {
+  const options = [
+    'Culture - CCS',
+    'Mobilité',
+    'Sports',
+    'DEE',
+    'Culture - CRR',
+  ]
+  const [email, setEmail] = useState('@strasbourg.eu')
   useEffect(() => {
-    axios.get(`${domain}/me`)
-    .then(res => res.data)
-    .then(info => {
-      if (info.email) {
-        setEmail(info.email)
-      }
-    })
+    axios
+      .get(`${domain}/me`)
+      .then((res) => res.data)
+      .then((info) => {
+        if (info.email) {
+          setEmail(info.email)
+        }
+      })
   }, [domain])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <h1>Application d’évaluation budgétaire dans le cadre de la refonte des grilles tarifaires de la Ville et l’Eurométropole de Strasbourg</h1>
-      { isOk(email) && (
-      <div>
-            <form method="post" encType="multipart/form-data" action={`${domain}/budget`}>
-              <fieldset>
+      <h1>
+        Application d’évaluation budgétaire dans le cadre de la refonte des
+        grilles tarifaires de la Ville et l’Eurométropole de Strasbourg
+      </h1>
+      {isOk(email) && (
+        <div>
+          <form
+            method="post"
+            encType="multipart/form-data"
+            action={`${domain}/budget`}
+          >
+            <fieldset>
               <legend>Évaluation budgétaire</legend>
               <div>
-              <label htmlFor="file">Fichier de barèmes : </label>
-              <input name="file"
-              id="file"
-              type="file" accept=".ods, application/vnd.oasis.opendocument.spreadsheet"
-              required/>
+                <label htmlFor="file">Fichier de barèmes : </label>
+                <input
+                  name="file"
+                  id="file"
+                  type="file"
+                  accept=".ods, application/vnd.oasis.opendocument.spreadsheet"
+                  required
+                />
               </div>
               <div>
-              <label htmlFor="subject">Thématique : </label>
-              <select name="subject" id="subject">
-                <option value="Culture - CCS">Culture - CCS</option>
-                <option value="Mobilité">Mobilité</option>
-                <option value="Sports">Sports</option>
-                <option value="DEE">DEE</option>
-                <option value="Culture - CRR">Culture - CRR</option>
-              </select>
+                <label htmlFor="subject">Thématique : </label>
+                <select name="subject" id="subject">
+                  {options.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
               </div>
-              <button type="submit">Envoyer</button>
-              </fieldset>
-            </form>
-      </div>
+              <button type="submit">Évaluer les scénarios</button>
+            </fieldset>
+          </form>
+        </div>
       )}
 
-      { !isOk(email) && (<div>
-        <p>Cette application nécessite une authentification avec une adresse email de Strasbourg.
-        </p>
-        { email && (<div>
-            <p>Adresse email utilisée : {email}</p>
-            <a href={`${domain}/logout`}>Se déconnecter</a>
-          </div>)}
-        { !email && (<a href={`${domain}/login`}><div className="moncomptepro-button"/></a> )}
+      {!isOk(email) && (
         <div>
           <p>
-          <a href="https://moncomptepro.beta.gouv.fr/" target="_blank" rel="noopener noreferrer"
-             title="Qu’est-ce que MonComptePro ? - nouvelle fenêtre">
-              Qu’est-ce que MonComptePro ?
-          </a>
-        </p>
+            Cette application nécessite une authentification avec une adresse
+            email de Strasbourg.
+          </p>
+          {email && (
+            <div>
+              <p>Adresse email utilisée : {email}</p>
+              <a href={`${domain}/logout`}>Se déconnecter</a>
+            </div>
+          )}
+          {!email && (
+            <a href={`${domain}/login`}>
+              <div className="moncomptepro-button" />
+            </a>
+          )}
+          <div>
+            <p>
+              <a
+                href="https://moncomptepro.beta.gouv.fr/"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Qu’est-ce que MonComptePro ? - nouvelle fenêtre"
+              >
+                Qu’est-ce que MonComptePro ?
+              </a>
+            </p>
+          </div>
         </div>
-        </div>)}
+      )}
     </main>
   )
 }
